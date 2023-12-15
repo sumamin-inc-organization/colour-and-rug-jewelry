966dfec37d90b084cc58b26188da0bdda7c95c52
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-15 15:26:08 +0900

    Webpackの設定を終わりました

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: "development",
-  output: {
-    assetModuleFilename: "images/[name].[ext]", //remove this and un comment the next line in distribution mode
-    // assetModuleFilename: "images/[name].[hash][ext]",
-    filename: "index.[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  devServer:{
-    static:{
-        directory:path.resolve(__dirname,'dist')
-    },
-    port:3000,
-    open:true,
-    hot:true,
-    compress:true,
-    historyApiFallback:true,
-},
+  mode:'development',
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'FUTURE L DAYS',
       filename:'index.html',
       template:'src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename:"[name].[contenthash].css"
+      filename:"[name].[contentHash].css"
     })
   ],
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    filename:"[name][contenthash].js",
+    clean:true,
+    assetModuleFilename:'./images/[name][ext]',
+},
+  devtool:'source-map',
+  devServer:{
+      static:{
+          directory:path.resolve(__dirname,'dist')
+      },
+      port:3000,
+      open:true,
+      hot:true,
+      compress:true,
+      historyApiFallback:true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
       {
         test: /\.html$/i,
         loader: "html-loader",
       },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-      },
     ],
   },
-
 };
