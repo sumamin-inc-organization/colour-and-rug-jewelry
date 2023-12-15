5a5554916b049cd7c60b74b7bb54fa1fdf058f4a
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-12-15 13:46:27 +0900

    GitHub Pagesを追加

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       template:'src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename:"[name].[contentHash].css"
+      filename:"[name].[contenthash].css"
     })
   ],
   module: {
