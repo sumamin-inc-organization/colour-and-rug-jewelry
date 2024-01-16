792c97e82853f297d1f5aff1cc231b720fd5552a
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-01-16 16:55:10 +0900

    GSAPを使用して企業名をアニメーション化しました。

--- src/index.js
+++ src/index.js
@@ -87,28 +87,28 @@ faqs.forEach((faq) => {
     Company Name  アニメーション
 ----------------------------*/
 
-const scrollers = document.querySelectorAll(".scroller"); //target the scrollers
+// const scrollers = document.querySelectorAll(".scroller"); //target the scrollers
 
 // if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 //   //check if user prefers reduced motion.( but inthis case it doesnt matter because its jus the logo)
 //   addAnimation();
 // }
 
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", "true");
+// function addAnimation() {
+//   scrollers.forEach((scroller) => {
+//     scroller.setAttribute("data-animated", "true");
 
-    const scrollerInner = scroller.querySelector(".scroller-inner"); //the ul
-    const scrollerContent = Array.from(scrollerInner.children); //get an array out of it all
+//     const scrollerInner = scroller.querySelector(".scroller-inner"); //the ul
+//     const scrollerContent = Array.from(scrollerInner.children); //get an array out of it all
 
-    scrollerContent.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true); //clone the children
-      duplicatedItem.setAttribute("aria-hidden", true); //add aria hidden attribute for screen readers
-      scrollerInner.appendChild(duplicatedItem); //append
-    });
-  });
-}
-addAnimation();
+//     scrollerContent.forEach((item) => {
+//       const duplicatedItem = item.cloneNode(true); //clone the children
+//       duplicatedItem.setAttribute("aria-hidden", true); //add aria hidden attribute for screen readers
+//       scrollerInner.appendChild(duplicatedItem); //append
+//     });
+//   });
+// }
+// addAnimation();
 /*----------------------------
     RECOMMENDED Animations
     RECOMMENDED  アニメーション
@@ -879,4 +879,145 @@ planAnitl
   .fromTo(".newplan-anime_4",{ y:0,duration:1.5},{y:"-100%"},"<")
   .fromTo(".newplan-anime_1",{ y:0,duration:1.5},{y:"100%"},"-=0.3")
   .fromTo(".newplan-anime_5",{ y:0,duration:1.5},{y:"100%"},"<")
-  .from(".newplan-anim-txt",{ y:100,opacity: 0,duration:1.5})
+  .from(".newplan-anim-txt",{ y:100,opacity: 0,duration:1.5});
+
+
+  function horizontalLoop(items, config) {
+    items = gsap.utils.toArray(items);
+    config = config || {};
+    let tl = gsap.timeline({
+        repeat: config.repeat,
+        paused: config.paused,
+        defaults: { ease: "none" },
+        onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100),
+      }),
+      length = items.length,
+      startX = items[0].offsetLeft,
+      times = [],
+      widths = [],
+      xPercents = [],
+      curIndex = 0,
+      pixelsPerSecond = (config.speed || 1) * 100,
+      snap = config.snap === false ? (v) => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+      totalWidth,
+      curX,
+      distanceToStart,
+      distanceToLoop,
+      item,
+      i;
+    gsap.set(items, {
+      // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+      xPercent: (i, el) => {
+        let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+        xPercents[i] = snap(
+          (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
+            gsap.getProperty(el, "xPercent")
+        );
+        return xPercents[i];
+      },
+    });
+    gsap.set(items, { x: 0 });
+    totalWidth =
+      items[length - 1].offsetLeft +
+      (xPercents[length - 1] / 100) * widths[length - 1] -
+      startX +
+      items[length - 1].offsetWidth *
+        gsap.getProperty(items[length - 1], "scaleX") +
+      (parseFloat(config.paddingRight) || 0);
+    for (i = 0; i < length; i++) {
+      item = items[i];
+      curX = (xPercents[i] / 100) * widths[i];
+      distanceToStart = item.offsetLeft + curX - startX;
+      distanceToLoop =
+        distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+      tl.to(
+        item,
+        {
+          xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
+          duration: distanceToLoop / pixelsPerSecond,
+        },
+        0
+      )
+        .fromTo(
+          item,
+          {
+            xPercent: snap(
+              ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
+            ),
+          },
+          {
+            xPercent: xPercents[i],
+            duration:
+              (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+            immediateRender: false,
+          },
+          distanceToLoop / pixelsPerSecond
+        )
+        .add("label" + i, distanceToStart / pixelsPerSecond);
+      times[i] = distanceToStart / pixelsPerSecond;
+    }
+    function toIndex(index, vars) {
+      vars = vars || {};
+      Math.abs(index - curIndex) > length / 2 &&
+        (index += index > curIndex ? -length : length); // always go in the shortest direction
+      let newIndex = gsap.utils.wrap(0, length, index),
+        time = times[newIndex];
+      if (time > tl.time() !== index > curIndex) {
+        // if we're wrapping the timeline's playhead, make the proper adjustments
+        vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
+        time += tl.duration() * (index > curIndex ? 1 : -1);
+      }
+      curIndex = newIndex;
+      vars.overwrite = true;
+      return tl.tweenTo(time, vars);
+    }
+    tl.next = (vars) => toIndex(curIndex + 1, vars);
+    tl.previous = (vars) => toIndex(curIndex - 1, vars);
+    tl.current = () => curIndex;
+    tl.toIndex = (index, vars) => toIndex(index, vars);
+    tl.times = times;
+    tl.progress(1, true).progress(0, true); // pre-render for performance
+    if (config.reversed) {
+      tl.vars.onReverseComplete();
+      tl.reverse();
+    }
+    return tl;
+  }
+
+
+  const listItem = gsap.utils.toArray(".list__item");
+  
+
+
+  // gsap.utils.toArray('.scroller2').forEach((line, i) => {
+  //   const links = line.querySelectorAll((".list__item"),
+  //         tl = horizontalLoop(links, {
+  //           repeat: -1, 
+  //           speed: 1 + i * 0.5,
+  //           reversed: i ? true : false,
+  //           paddingRight: parseFloat(gsap.getProperty(links[0], "marginRight", "px")) // otherwise first element would be right up against the last when it loops. In this layout, the spacing is done with marginRight.
+  //         });
+  //   // links.forEach(link => {
+  //   //   link.addEventListener("mouseenter", () => gsap.to(tl, {timeScale: 0, overwrite: true}));
+  //   //   link.addEventListener("mouseleave", () => gsap.to(tl, {timeScale: i ? -1 : 1, overwrite: true}));
+  //   // });
+  // });
+
+
+  function flowText(scrollerClass,itemClass){
+
+    gsap.utils.toArray(scrollerClass).forEach((line, i) => {
+      const links = line.querySelectorAll(itemClass),
+            tl = horizontalLoop(links, {
+              repeat: -1, 
+              speed: 1 + i * 0.5,
+              reversed: i ? true : false,
+              paddingRight: parseFloat(gsap.getProperty(links[0], "marginRight", "px")) // otherwise first element would be right up against the last when it loops. In this layout, the spacing is done with marginRight.
+            });
+    });
+  }
+
+  flowText('.scroller1',".list__item");
+  flowText(".scroller2",".list__item");
+  
+  
