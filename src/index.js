92b8168cf0c68913ef85480737fe17f10195e443
evilkeil <114153280+evilkeil@users.noreply.github.com>
2024-01-17 17:37:13 +0900

    PCの場合のみ、ハンバーガーアイコンの色がKVセクションで変更されるようにしました。

--- src/index.js
+++ src/index.js
@@ -677,8 +677,10 @@ ScrollTrigger.create({
 });
 
 //trigger for when the right side hits the kv bottom image
-changeOnlyRight(".kv_02");
 
+if(isItPc.matches){
+  changeOnlyRight(".kv_02");
+}
 
 // newplan trigger
 ScrollTrigger.create({
